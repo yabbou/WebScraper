829ceccfda96455568ee2ed16cfafd13950d5f75
Jacob Abbou <36824257+yabbou@users.noreply.github.com>
2019-05-31 11:54:01 -0400

    Add multi-threading concurrency

--- edu/mco364/WebCrawler.java
+++ edu/mco364/WebCrawler.java
@@ -6,54 +6,58 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-class WebCrawler{
-    private Set<String> totalEmails = new HashSet<>();
-    private List<String> totalLinks = new ArrayList<>();
-    private int linkIndex = 0;
+class WebCrawler implements Runnable {
 
-    Set<String> getTotalEmails() {
+    WebCrawler(final int MAX_EMAIL, String ROOT_URL) {
+        WebCrawler.MAX_EMAIL = MAX_EMAIL;
+        totalLinks.add(ROOT_URL);
+    }
+
+    private static int MAX_EMAIL;
+    private static Set<String> totalEmails = Collections.synchronizedSet(new HashSet<>(MAX_EMAIL));
+    private static Queue<String> totalLinks = new ConcurrentLinkedQueue<>();
+
+    static Set<String> getTotalEmails() {
         return totalEmails;
     }
 
-    void beginBreadthFirstSearch(String URL) {
-        totalLinks.add(URL);
-        breadthFirstSearchForEmails(totalLinks.get(linkIndex++));
-    }
-
     private void breadthFirstSearchForEmails(String URL) {
-        final int MAX_EMAIL_COUNT = 200; 
         final int INDEX_OF_EMAIL = 1;
-        String currentURL;
+        String currentLink;
 
         try {
-            Document rootURL = Jsoup.connect(URL)
+            Document website = Jsoup.connect(URL)
                     .userAgent("Chrome")
                     .get();
-            Elements currentLinks = rootURL.select("a[href]");
+            Elements currentLinks = website.select("a[href]");
 
             for (Element link : currentLinks) {
-                currentURL = link.attr("abs:href");
+                currentLink = link.attr("abs:href");
 
-                if (!totalLinks.contains(currentURL)) {
-                    if (!currentURL.startsWith("mailto:"))
-                        totalLinks.add(currentURL);
-                    else if (totalEmails.size() < MAX_EMAIL_COUNT) {
-                        totalEmails.add(currentURL.split("mailto:")[INDEX_OF_EMAIL]);
+                if (!totalLinks.contains(currentLink)) {
+                    if (!currentLink.startsWith("mailto:"))
+                        totalLinks.add(currentLink);
+                    else if (totalEmails.size() < MAX_EMAIL) {
+                        totalEmails.add(currentLink.split("mailto:")[INDEX_OF_EMAIL]);
                     }
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (totalEmails.size() < MAX_EMAIL_COUNT) {
-            breadthFirstSearchForEmails(totalLinks.get(linkIndex++));
+        if (totalEmails.size() < MAX_EMAIL) {
+            breadthFirstSearchForEmails(totalLinks.remove());
+        }
+    }
+
+    @Override
+    public void run() {
+        synchronized (totalLinks) {
+            breadthFirstSearchForEmails(totalLinks.remove());
         }
     }
 }
